739628648ccf8c7e98a7d9612ad09a7badc406e1
Silvestre Espinoza <90989842+heeeeeeeeeeh@users.noreply.github.com>
2026-04-26 13:03:23 -0500

    Add moviesingle.html from zip file

--- filmReviewApp/urls.py
+++ filmReviewApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="index"),
-               path("login/", views.login, name="login"),
-               path("signup/", views.signup, name="signup"),
-               path("movielist/", views.movielisting, name="movielist"),
-               path("moviesingle/<int:id>/", views.movesingle, name="movesingle")
-               ]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
+    path("signup/", views.signup, name="signup"),
+    path("movielist/", views.movielisting, name="movielist"),
+    path("moviesingle/", views.moviesingle, name="moviesingle"),
+]
